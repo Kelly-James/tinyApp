811b5607fb9055202db2416ec27298e7471af84b
Kelly James Clancy <kellyjamesclancy@gmail.com>
2016-10-24 01:51:39 +0000

    added views: error, header, added functionality: login, register, cookies

--- expressServer.js
+++ expressServer.js
@@ -15,7 +15,7 @@ var urlDatabase = {
 };
 
 var users = {
-  'userID':{id:"", email:123 , password: 123}
+  'userID':{id: '', email: '', password: ''}
 };
 
 function generateRandomString() {
@@ -35,80 +35,32 @@ function prefixCheck(url) {
   }
 };
 
-// get welcome
+// // get welcome
 app.get('/', (req, res) => {
-  res.render('login');
-})
-
-// get index
-app.get('/index', (req, res) => {
-  res.render('index');
+  let userID = req.cookies['userID'];
+  let userProfile = users[userID];
+  let currentUser = {id: userProfile.id,
+                    email: userProfile.email,
+                    password: userProfile.password};
+  console.log(currentUser);
+  res.render('login', currentUser);
 });
 
 // get create
 app.get('/create', (req, res) => {
-  res.render('create');
+  let userID = req.cookies['userID'];
+  let userProfile = users[userID];
+  let userEmail = {email: userProfile.email};
+  res.render('create', userEmail);
 });
 
-// get registration
-app.get('/register', (req, res) => {
-  res.render('register');
-});
-
-// handler
-app.post('/register', (req, res) => {
-  randomID = generateRandomString()
-  users[randomID] = {
-    'id': randomID,
-    'email': req.body.email,
-    'password': req.body.password};
-  console.log(users);
-  res.render('index');
-});
-
-// mmmm, cookies handler
-app.post('/', (req, res) => {
-  var username = req.body.name
-  res.cookie('username', req.body.username, {maxAge: 86400000});
-  res.render('index.ejs');
-})
-
-// get list
-app.get('/list', (req, res) => {
-  let templateVars = {urls: urlDatabase};
-  res.render('list', templateVars);
-});
-
-// call generateRandomString on input, post output to list
-app.post('/list', (req, res) => {
-  urlDatabase[generateRandomString()] = prefixCheck(req.body.longURL);
-  res.redirect('/list');
-});
-
-
-app.post('/list/:id', (req, res) => {
-  var longURL = req.params.id;
-  fun.apply(this.longURL, urlDatabase);
-  res.redirect('/list');
-});
-
-// delete a url
-app.post('/list/:id/delete', (req, res) => {
-  var shortURL = req.params.id;
-  delete urlDatabase[shortURL];
-  res.redirect('/list');
-});
-
-// diplay a form to update url with id of req.params.id
-app.get('/update/:id', (req, res) => {
-  let templateVars = {shortURL: req.params.id};
-  res.render('update', templateVars);
-});
-
-// make changes to the url and save it
-app.post('/update/:id', (req, res) => {
-    urlDatabase[req.params.id] = prefixCheck(req.body.longURL);
-  res.redirect('/list');
+// get index
+app.get('/index', (req, res) => {
+  let userID = req.cookies['userID'];
+  let userProfile = users[userID];
+  let userEmail = {email: userProfile.email};
+  console.log(userEmail);
+  res.render('index', userEmail);
 });
 
 app.get('/hello', (req, res) => {
@@ -117,6 +69,91 @@ app.get('/hello', (req, res) => {
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
+});
+
+// get list
+app.get('/list', (req, res) => {
+  let userID = req.cookies['userID'];
+  let userProfile = users[userID];
+  let userEmail = {email: userProfile.email}
+  let templateVars = {urls: urlDatabase,
+                      email: userProfile.email};
+  res.render('list', templateVars);
+});
+
+// get registration
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+// diplay a form to update url
+app.get('/update/:id', (req, res) => {
+  let userID = req.cookies['userID'];
+  let userProfile = users[userID];
+  let userEmail = {email: userProfile.email}
+  let templateVars = {shortURL: req.params.id,
+                      email: userProfile.email};
+  res.render('update', templateVars);
+});
+
+// login
+app.post('/', (req, res) => {
+  let userID = req.cookies['userID'];
+  let userProfile = users[userID];
+  let currentUser = {id: userProfile.id};
+  console.log(currentUser);
+  res.cookie('username', req.body.username, {maxAge: 86400000});
+  let username = req.body.username;
+  console.log(username);
+  if(username !== currentUser.id) {
+    res.render('error');
+  } else if (currentUser.id == null) {
+    res.redirect('register');
+  } else {
+    res.redirect('index');
+  }
+});
+
+// logout not functional
+app.post('/_header', (req, res) => {
+  res.clearCookie('userID', {path: '/'});
+  res.redirect('/');
+});
+
+// call generateRandomString on input, post output to list
+app.post('/list', (req, res) => {
+  urlDatabase[generateRandomString()] = prefixCheck(req.body.longURL);
+  res.redirect('/list');
+});
+
+app.post('/list/:id', (req, res) => {
+  let longURL = req.params.id;
+  fun.apply(this.longURL, urlDatabase);
+  res.redirect('/list');
+});
+
+// delete a url
+app.post('/list/:id/delete', (req, res) => {
+  let shortURL = req.params.id;
+  delete urlDatabase[shortURL];
+  res.redirect('/list');
+});
+
+// register new user
+app.post('/register', (req, res) => {
+  randomID = generateRandomString()
+  users[randomID] = {
+    'id': randomID,
+    'email': req.body.email,
+    'password': req.body.password};
+  res.cookie('userID', randomID, {maxAge: 86400000});
+  res.redirect('index');
+});
+
+// make changes to the url and save it
+app.post('/update/:id', (req, res) => {
+    urlDatabase[req.params.id] = prefixCheck(req.body.longURL);
+  res.redirect('/list');
 });
 
 app.listen(PORT, () => {
